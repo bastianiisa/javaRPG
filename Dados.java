3b83b4745dcef9f0c6e7f9c22cc503481f7604db
bastianiisa <145716438+bastianiisa@users.noreply.github.com>
2024-12-18 20:12:41 -0300

    Dados.java

--- Dados.java
+++ Dados.java
@@ -2,14 +2,12 @@ import java.util.Scanner;
 
 public class Dados {
 
-     // Método que pede ao jogador para rolar os dados
     public static int rolarDados(Scanner scanner, int quantidade, int faces) {
-        int resultadoTotal = 0; // variável que vai somar o valor total dos dados, dependendo da quantidade
+        int resultadoTotal = 0;
         for (int i = 1; i <= quantidade; i++) {
             System.out.println("Pressione Enter para rolar o dado " + i + " de " + quantidade + " (d" + faces + ").");
-            scanner.nextLine(); // Aguarda o Enter do jogador
-             // Gerar valor entre 1 e o número máximo de lados (sides)
-            int resultado = (int) (Math.random() * faces) + 1; // o + 1 garante que o valor seja, no mínimo, 1, e não zero. O (int)  descarta a parte decimal, deixando apenas o número inteiro
+            scanner.nextLine(); 
+            int resultado = (int) (Math.random() * faces) + 1; 
             System.out.println("Você rolou: " + resultado);
             resultadoTotal += resultado;
         }
